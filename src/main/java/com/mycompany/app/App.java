5e1b7e642a5e349544a3fc024a5f865acfa70948
Anderson Araujo <anderson.araujo@gmail.com>
2016-05-06 11:01:48 -0300

    Testing coverage

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -2,8 +2,10 @@ package com.mycompany.app;
 
 public class App {
     public static void main(String args[]) {
-        if(true){
+        if (args != null) {
             System.out.println("hello world");
+        } else {
+            System.out.println("hello world - else");
         }
     }
 }
---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,15 @@
+package com.mycompany.app;
+
+import org.junit.Test;
+
+/**
+ * Created by Anderson Araujo.
+ */
+public class AppTest {
+
+    @Test
+    public void test() {
+        new App().main(new String[]{""});
+    }
+
+}
